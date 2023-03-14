b54a99e07260b19ef71c0267ed64bedb14d8ff83
Monoget <monoget2@gmail.com>
2023-03-14 17:55:06 +0600

    update design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,43 @@
-let swiper = new Swiper(".swiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 100,
-        modifier: 2,
-        slideShadows: true
-    },
-    spaceBetween: 60,
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-    }
+$(document).ready(function(){
+    let swiper = new Swiper(".swiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 2,
+            slideShadows: true
+        },
+        spaceBetween: 60,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        }
+    });
+
+
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
 });
